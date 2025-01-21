c6cb256906e1122521fcc153587ac281b63b1553
AlexisEgea <alexisegea@oulook.com>
2025-01-21 01:04:12 +0100

    Updating script by using conda instead of venv

--- installation-requirements.sh
+++ installation-requirements.sh
@@ -16,20 +16,21 @@ else
 fi
 
 echo "_____________________________________________________________________________"
-echo "creation venv repository..."
-$PYTHON_CMD -m venv venv
-echo "...done"
-echo "_____________________________________________________________________________"
-echo "activation venv..."
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	source venv/bin/activate
-elif [[ "$OSTYPE" == "cygwin"* || "$OSTYPE" == "msys"* ]]; then
- 	venv/Scripts/activate
+echo "activation of conda..."
+CONDA_PATH="$HOME/anaconda3/etc/profile.d/conda.sh"
+if [ -f "$CONDA_PATH" ]; then
+    source "$CONDA_PATH"
+else
+    echo "Conda not found at $CONDA_PATH"
+    exit 1
 fi
 
+read -p "Enter the name of your conda env: " env
+conda activate $env
+
 echo "...done"
 echo "_____________________________________________________________________________"
+
 echo "installation requirement.txt"
 pip install -r requirement.txt
 echo "Done, project ready to be executed"
